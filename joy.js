0e16a515d59866193042b80a97ce21830493531c
bobboteck <bobboteck@gmail.com>
2015-06-30 18:33:38 +0200

    Same fixes

--- joy.js
+++ joy.js
@@ -1,25 +1,30 @@
 /*!
  * Name          : joy.js
- * Authors       : Roberto D'Amico
+ * Authors       : Roberto D'Amico (Bobboteck)
  * Last modified : 26.06.2015
  * Revision      : 1.0.0
  *
- * Copyright (c) 2015, Roberto D'Amico
- * All rights reserved.
+ * The MIT License (MIT)
  *
- *  Redistribution and use in source and binary forms, with or without modification, are permitted
- *  provided that the following conditions are met:
+ * Copyright (c) 2015 Roberto D'Amico (Bobboteck)
  *
- *  # Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- *  # Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided with the distribution.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
- *   BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- *   SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- *   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 var JoyStick = (function(container, parameters) {
 	parameters = parameters || {};
@@ -30,7 +35,7 @@ var JoyStick = (function(container, parameters) {
 		internalLineWidth = (undefined === parameters.internalLineWidth ? 5 : parameters.internalLineWidth),
 		internalStrokeStyle = (undefined === parameters.internalStrokeStyle ? '#003300' : parameters.internalStrokeStyle),
 		externalLineWidth = (undefined === parameters.externalLineWidth ? 2 : parameters.externalLineWidth),
-		externalStrokeStyle = (undefined === parameters.internalStrokeStyle ? '#003300' : parameters.internalStrokeStyle);
+		externalStrokeStyle = (undefined === parameters.externalStrokeStyle ? '#003300' : parameters.externalStrokeStyle);
 	
 	// Create Canvas element and add it in the Container object
 	var objContainer = document.getElementById(container);
@@ -73,9 +78,9 @@ var JoyStick = (function(container, parameters) {
 		// create radial gradient
 		var grd = context.createRadialGradient(movedX, movedY, 10, movedX, movedY, 80);
 		// Light color
-		grd.addColorStop(0, 'green');
+		grd.addColorStop(0, internalFillStyle);
 		// Dark color
-		grd.addColorStop(1, '#003300');
+		grd.addColorStop(1, internalFillStyle);
 		context.fillStyle = grd;
 		context.fill();
 		context.lineWidth = internalLineWidth;
@@ -167,12 +172,12 @@ var JoyStick = (function(container, parameters) {
 	this.getPosX = function ()
 	{
 		return movedX;
-	}
+	};
 
 	this.getPosY = function ()
 	{
 		return movedY;
-	}
+	};
 	
 	this.getDir = function()
 	{
@@ -197,7 +202,7 @@ var JoyStick = (function(container, parameters) {
 		{
 			if(result=="C")
 			{ 
-				result = "W" 
+				result = "W";
 			}
 			else
 			{
@@ -208,7 +213,7 @@ var JoyStick = (function(container, parameters) {
 		{
 			if(result=="C")
 			{ 
-				result = "W" 
+				result = "W";
 			}
 			else
 			{
@@ -217,7 +222,7 @@ var JoyStick = (function(container, parameters) {
 		}
 		
 		return result;
-	}
+	};
 	
 	// Check if the device support the touch or not
 	var touchable = 'createTouch' in document;
